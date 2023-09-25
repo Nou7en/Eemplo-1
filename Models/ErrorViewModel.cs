bd111bed5ad43ba598ae91de80911f63400eb41e
Nou7en <tomh.tc89@gmail.com>
2023-09-25 08:29:25 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Eemplo_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
